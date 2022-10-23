9fd748c24222f4edd956dd606b22e601a4047364
parkdin <abdukokhorovizatillo00@gmail.com>
2022-10-23 21:11:17 +0900

    func19 s done

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,8 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
+def main(a, b):
+  c = a - b
+  c = c - c * 2
+  return c
+print(main(40,11))
